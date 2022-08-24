eeca8c0308ce56de38dc61cc35c723973f9c3428
Francisco Oliveira <froliveira@outlook.com>
2022-08-24 10:34:39 -0300

    refactor: change folder structure

---
+++ src/catalogo/domain/produto.ts
@@ -0,0 +1,53 @@
+import { IAggregateRoot } from 'src/core/domainObjects/aggregate-root.interface';
+import { Entity } from 'src/core/domainObjects/entity';
+import { Categoria } from './categoria';
+import { Dimensoes } from './dimensoes';
+
+export class Produto extends Entity implements IAggregateRoot {
+  categoriaId: string;
+  nome: string;
+  descricao: string;
+  ativo: boolean;
+  valor: number;
+  dataCadastro: Date;
+  quantidadeEstoque: number;
+  dimensoes: Dimensoes;
+  categoria: Categoria;
+
+  constructor(
+    nome: string,
+    descricao: string,
+    ativo: boolean,
+    valor: number,
+    dataCadastro: Date,
+    dimensoes: Dimensoes,
+  ) {
+    super();
+
+    this.nome = nome;
+    this.descricao = descricao;
+    this.ativo = ativo;
+    this.valor = valor;
+    this.dataCadastro = dataCadastro;
+    this.dimensoes = dimensoes;
+
+    //TODO: adicionar forma de validação
+    this.validar();
+  }
+
+  public debitarEstoque(quantidade: number) {
+    this.quantidadeEstoque -= quantidade;
+  }
+
+  public reporEstoque(quantidade: number) {
+    this.quantidadeEstoque += quantidade;
+  }
+
+  public possuiEstoque(quantidade: number) {
+    return this.quantidadeEstoque >= quantidade;
+  }
+
+  public validar() {
+    return true;
+  }
+}
